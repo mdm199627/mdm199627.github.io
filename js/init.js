581d3543a1b04c426414eb458c7f0c0c8c317d2b
mdm199627 <76072866+mdm199627@users.noreply.github.com>
2021-01-23 17:34:35 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,8 @@
+firebase.initializeApp({
+apiKey: "AIzaSyDtXdSdfy8eVNJSsaFrqhvzSmbWJPz0eBU",
+    authDomain: "mdm199627fbweb.firebaseapp.com",
+    projectId: "mdm199627fbweb",
+    storageBucket: "mdm199627fbweb.appspot.com",
+    messagingSenderId: "873162475850",
+    appId: "1:873162475850:web:1ab45394eb818e985999b3"
+});
