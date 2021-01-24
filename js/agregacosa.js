7c5662207b928693aeaea7fe8d2656ddf4e342f2
mdm199627 <76072866+mdm199627@users.noreply.github.com>
2021-01-24 11:58:54 -0600

    Create agregacosa.js

---
+++ js/agregacosa.js
@@ -0,0 +1,29 @@
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+var nombrei = document.getElementById("nombreProducto").value;
+var des = document.getElementById("descri").value;
+var canti = document.getElementById("cantidad").value;
+var identipro = document.getElementById("identiPro").value;
+var medi = document.getElementById("medicion").value;
+var consumo = document.getElementById("consumotipo").value;
+var idinve = document.getElementById("InvenProid").value;
+var forma = document.getElementById("agregar");
+forma.addEventListener("submit",agrega(),false);
+function agrega(){
+  db.collection("Producto").add({
+    cantidad: canti,
+    descripcion: des,
+    idinve: idinve ,
+    idpro: identipro,
+    medicion: medi,
+    nombre: nombrei,
+    tipo: consumo
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});  
+}
